e755ac0f2062812acdb181cb0fe4d8d86bf5a363
Моргунов Евгений <104060685+EvgeniyMorgan@users.noreply.github.com>
2025-08-02 23:46:09 +0300

    Add files via upload

---
+++ demo/unit1.pas
@@ -0,0 +1,50 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  AlignButtonContainer;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    AlignButtonContainer1: TAlignButtonContainer;
+    AlignButtonContainer2: TAlignButtonContainer;
+    AlignButtonContainer3: TAlignButtonContainer;
+    AlignButtonContainer4: TAlignButtonContainer;
+    AlignButtonContainer5: TAlignButtonContainer;
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
